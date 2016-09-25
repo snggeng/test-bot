024cbe070fa570ac871eb859221f4837fdf20ac8
Geng <snggeng@gmailcom>
2016-09-25 01:53:58 -0400

    change setTimeout to setInterval

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/webhook/', function (req, res) {
 			}
 			if (text === 'haircut') {
 				sendUniqueMessage(sender, "There are 5 hair stylists available now.")
-				setTimeOut(sendQuickReplyMessage(sender), 500)
+				setInterval(sendQuickReplyMessage(sender), 500)
 				continue
 			}
 			if (text === 'Gordon Levitt') {
